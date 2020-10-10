dc1d3f35172ee0d04f73f79c3e112a25b5c10dc3
Nadia <nadezda.mitkaleva@gmail.ru>
2020-10-10 18:19:35 +0500

    Взаимодействие с удаленным сервером

---
+++ js/load.js
@@ -0,0 +1,33 @@
+'use strict';
+
+(function () {
+  let URL_LOAD = `https://21.javascript.pages.academy/keksobooking/data`;
+  let StatusCode = {
+    OK: 200
+  };
+
+  window.load = function (onSuccess, onError) {
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = `json`;
+
+    xhr.addEventListener(`load`, function () {
+      if (xhr.status === StatusCode.OK) {
+        onSuccess(xhr.response);
+      } else {
+        onError(`Статус ответа: ` + xhr.status + ` ` + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener(`error`, function () {
+      onError(`Произошла ошибка соединения`);
+    });
+    xhr.addEventListener(`timeout`, function () {
+      onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open(`GET`, URL_LOAD);
+    xhr.send();
+  };
+})();
